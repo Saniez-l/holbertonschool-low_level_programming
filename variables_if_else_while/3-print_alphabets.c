71149fe2d4e3886bce2906bbfa9d186b14a5e2c8
Saniez-l <saniezl.christophe@gmail.com>
2025-06-17 21:56:21 +0200

    program display alfabet in lowercase ans uppercase in C

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lettres;
+
+	for (lettres = 'a'; lettres <= 'z'; lettres++)
+		putchar(lettres);
+
+	for (lettres = 'A'; lettres <= 'Z'; lettres++)
+		putchar(lettres);
+
+	putchar('\n');
+
+	return (0);
+}
