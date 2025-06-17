12600e8454f728d815d39c22adf9128421a92fad
Saniez-l <saniezl.christophe@gmail.com>
2025-06-17 17:11:48 +0200

    display the last digit of one number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-	int n, lastDigit;
+	int n, ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	lastDigit = n % 10;
 
-	if (n > 5 && n != 0)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	else if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 
 	return (0);
 }
