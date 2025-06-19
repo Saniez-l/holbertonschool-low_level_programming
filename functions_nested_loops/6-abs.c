2f39953dcf84c5216d7df71c4bb33440fd2b4921
Saniez-l <saniezl.christophe@gmail.com>
2025-06-19 22:57:22 +0200

    program c define the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - n is integer
+ * @n : caractere n
+ *
+ * Return: number if number is pisitve -number otherwise
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+
+}
