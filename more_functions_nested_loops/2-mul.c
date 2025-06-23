982eb1159641dab8f59a4126a0fc6d6ed318c995
Saniez-l <saniezl.christophe@gmail.com>
2025-06-23 11:01:14 +0200

    program return multiplication of 2 number

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplication 2 number
+ * @a : number a
+ * @b : number b
+ *
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	int resultat;
+
+	resultat = a * b;
+	return (resultat);
+}
