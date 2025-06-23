c31e3cfba95595e8c51efd12b7665b893c8f9c24
Saniez-l <saniezl.christophe@gmail.com>
2025-06-23 16:58:46 +0200

    program display square for size*

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - display square
+ * @size : number size
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int i, x;
+
+	for (i = 0; i < size; i++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
+
