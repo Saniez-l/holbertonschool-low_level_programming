2f4b002854bf1c947615a06d4fcd8eb2c384023b
Saniez-l <saniezl.christophe@gmail.com>
2025-06-23 18:09:59 +0200

    program FizzBuZZ print 1 of 100 , if multiple de 3 and 5,3,5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main- check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nombre;
+
+	for (nombre = 1; nombre <= 100; nombre++)
+	{
+		if (nombre % 3 == 0 && nombre % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (nombre % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (nombre % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", nombre);
+		}
+		if (nombre < 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
