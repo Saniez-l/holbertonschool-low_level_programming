e03e4858143d8c544efc0db7ebef2f187ab2ba5c
Saniez-l <saniezl.christophe@gmail.com>
2025-06-23 14:30:15 +0200

    add betty style

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
+/**
+ * more_numbers - print number 0 to 14 * 10
+ * return 0
+ */
 void more_numbers(void)
 {
-	int number,fois_dix;
+	int number, fois_dix;
 
 	for (fois_dix = 0; fois_dix < 10; fois_dix++)
 	{
 		for (number = 0; number <= 14; number++)
 		{
-			if (number >9)
+			if (number > 9)
 			{
-			
 				_putchar('0' + number / 10);
 			}
 			_putchar('0' + number % 10);
