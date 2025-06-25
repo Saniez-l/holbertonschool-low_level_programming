4da388df4cd2ec32bc4037dbd8b9b83f00e3086c
Saniez-l <saniezl.christophe@gmail.com>
2025-06-25 12:59:39 +0200

    add betty style

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,25 @@
 #include "main.h"
 
+/**
+ * puts_half - print string midel string
+ * @str : char str
+ *
+ * Return: 0
+ */
 void puts_half(char *str)
 {
 	int start = 0, end = 0;
 
-	while(str[end] != '\0')
+	while (str[end] != '\0')
 		end++;
 	end--;
-	while(start < end)
+	while (start < end)
 	{
 		start++;
 		end--;
 	}
-	end +=1;
-	while(str[end] != '\0')
+	end += 1;
+	while (str[end] != '\0')
 	{
 		_putchar(str[end]);
 		end++;
