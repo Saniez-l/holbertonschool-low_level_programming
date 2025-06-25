84997c3f3d5f4f2a0c4887a3805c9330a42438c6
Saniez-l <saniezl.christophe@gmail.com>
2025-06-25 15:59:48 +0200

    print integral of number

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef function
 #define function
 
+#include <stdio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,5 +12,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif
