67c0a57108649cb22f99ecd172c55dd336ac8f04
Saniez-l <saniezl.christophe@gmail.com>
2025-06-25 11:27:28 +0200

    program print at the midel of string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int start = 0, end = 0;
+
+	while(str[end] != '\0')
+		end++;
+	end--;
+	while(start < end)
+	{
+		start++;
+		end--;
+	}
+	end +=1;
+	while(str[end] != '\0')
+	{
+		_putchar(str[end]);
+		end++;
+	}
+	_putchar('\n');
+}
