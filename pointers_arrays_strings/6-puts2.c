a9313d29978743189bbec6d2f7ec7a1fde438a94
Saniez-l <saniezl.christophe@gmail.com>
2025-06-25 09:26:19 +0200

    program to print one car on 2 on string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print 1 char on 2
+ * @str : string
+ *
+ * Return 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar (str[i]);
+		i += 2;
+	}
+	_putchar ('\n');
+}
