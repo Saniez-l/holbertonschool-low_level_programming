2b0505003874b9f89812ba83553111734d983813
Saniez-l <saniezl.christophe@gmail.com>
2025-06-30 18:44:14 +0200

    program compare 2 string and return the end string when it s the same

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, i1 = 0;
+
+	while (s[i] != '\0')
+	{
+		i1 = 0;
+		while (accept[i1] != '\0')
+		{
+			if (s[i] == accept[i1])
+			{
+				return (&s[i]);
+			}
+			i1++;
+		}
+		i++;
+	}
+	return (0);
+}
