555615b50f588d3b6dcfc19caf352915872ee4b9
Saniez-l <saniezl.christophe@gmail.com>
2025-06-30 15:58:31 +0200

    program cunt the same caracter of two string on the first word

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - cunt same char of the first world on the string
+ * @accept: string
+ * @s: string
+ *
+ * Return: cunt
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, i1 = 0, cunt = 0;
+
+	while (accept[i1] != '\0')
+	{
+		while (s[i] != 32)
+		{
+			if (s[i] == accept[i1])
+				cunt = i + 1;
+			i++;
+		}
+		i1++;
+	}
+	return (cunt);
+}
+
