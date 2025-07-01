39fa762cd67f9a81848b869b68cc50e7c7995f61
Saniez-l <saniezl.christophe@gmail.com>
2025-07-01 21:53:05 +0200

    add betty style

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - lenght string
+ * @s: string
+ *
+ * Return: recursion
+ */
 int _strlen_recursion(char *s)
 {
 
@@ -7,6 +13,5 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
-		return (1 + _strlen_recursion (s + 1));
-	
+		return (1 + _strlen_recursion(s + 1));
 }
