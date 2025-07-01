6779bd0a74602000c2e383404dbf43196f05c0e7
Saniez-l <saniezl.christophe@gmail.com>
2025-07-01 10:36:01 +0200

    add betty style

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+/**
+ * _strstr - first occurance of the string
+ * @haystack: string
+ * @needle: occurance string
+ *
+ * Return: haystack
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j = 0;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0;needle[j] !='\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
 				break;
@@ -17,4 +24,3 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return (0);
 }
-						
