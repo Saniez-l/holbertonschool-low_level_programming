6b378951e6229547829114f5ea66b4289ef9f6cc
Saniez-l <saniezl.christophe@gmail.com>
2025-07-02 18:34:20 +0200

    change program to print other main

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,15 +3,21 @@
 /**
  * _puts_recursion - print string
  * @s: strings
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		 _putchar(*s);
+        	_puts_recursion(s + 1);
+		/*_putchar('\n');*/
+		/*return;*/
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	/*_putchar(*s);
+	_puts_recursion(s + 1);*/
+	if(*s == '\0')	
+	_putchar('\n');
 }
 
