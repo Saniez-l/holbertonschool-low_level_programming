1f9c842ad35f72cd561b316fc3f56272402e2912
Saniez-l <saniezl.christophe@gmail.com>
2025-07-07 23:30:14 +0200

    program alloc memory for 2d array

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - alloc arraw 2d
+ * @width: width raw
+ * @height: height raw
+ *
+ * Return: s
+ * Return : NULL size null
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j = 0;
+	int **s = 0;
+
+	if (width <= 0 || height <= 0)
+	{
+	return (NULL);
+	}
+	s = malloc(sizeof(int) * (height));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		s[i] = malloc(sizeof(int) * width);
+		if (s[i] == NULL)
+		{
+			return (0);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			s[i][j] = 0;
+		}
+	}
+	return (s);
+}
