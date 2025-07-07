9449938eaef7971690d20430a56870642a7887d4
Saniez-l <saniezl.christophe@gmail.com>
2025-07-07 15:42:36 +0200

    program duplicate string with malloc

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - array with maloc
+ * @str: array
+ *
+ * Return: s
+ * Return : NULL size null
+ */
+char *_strdup(char *str)
+{
+	char *s = 0;
+	int i = 0, j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	s = malloc(j * sizeof(char) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < j; i++)
+	{
+					s[i] = str[i];
+	}
+	return (s);
+}
