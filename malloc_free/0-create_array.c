9dae5bc9291719169a0ff2aec96a06ed97114e98
Saniez-l <saniezl.christophe@gmail.com>
2025-07-07 14:38:01 +0200

    correction initialisation pointer

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	s = malloc(size * sizeof(c));
+	s = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
