bfced79554eff2b14f3667f5e8fc6db81a9fa641
Saniez-l <saniezl.christophe@gmail.com>
2025-07-07 14:26:30 +0200

    program print array with malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,19 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_array - array with maloc
+ * @c: array
+ * @size : of array
+ *
+ * Return: s
+ * Return NULL: size null
+ */
 char *create_array(unsigned int size, char c)
 {
-    char *s;
-    unsigned int i = 0;
+	char *s;
+	unsigned int i = 0;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
-    s = malloc(size * sizeof(c));
-    for (i = 0; i < size; i++)
-    {
-        s[i] = c;
-    }
-    return (s);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc(size * sizeof(c));
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
 }
