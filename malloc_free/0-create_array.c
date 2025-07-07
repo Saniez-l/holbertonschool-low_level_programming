d4f8cbb3079b111e8467660fdf8e9749a64342d6
Saniez-l <saniezl.christophe@gmail.com>
2025-07-07 14:16:59 +0200

    program print array with malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    char *s;
+    unsigned int i = 0;
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    s = malloc(size * sizeof(c));
+    for (i = 0; i < size; i++)
+    {
+        s[i] = c;
+    }
+    return (s);
+}
