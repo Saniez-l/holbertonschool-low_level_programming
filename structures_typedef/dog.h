35cc27f071620d96cbc80307cf94f3f3dc903a92
Saniez-l <saniezl.christophe@gmail.com>
2025-07-09 16:15:07 +0200

    struct for information of dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - struct for dog
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ *
+ * Description: precision for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
