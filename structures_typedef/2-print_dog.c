4c41e142c5725ad2463ac2bce24db07499662500
Saniez-l <saniezl.christophe@gmail.com>
2025-07-09 21:57:45 +0200

    function print element of dog with struct

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include<stdio.h>
+
+/**
+ * print_dog - print element of dog
+ * @d: d pointeur element struct
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d->age == 0 || d->owner == 0)
+	{
+		printf("(nil)\n");
+	}
+	if (d->name == 0)
+	{
+		printf("Name: (nil)\n");
+	}
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
