bfe8cdf8f679c9e9ae54235d8949f37f40e9c57c
Saniez-l <saniezl.christophe@gmail.com>
2025-07-10 14:06:38 +0200

    function create new dog update correstion code if no name or owner or age

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "dog.h"
 
@@ -20,15 +21,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-
-	if (new_dog == NULL)
+		if (new_dog == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+
+	new_dog->name = name;
+	new_dog->owner = owner;
+	new_dog->age = age;
 
 	return (new_dog);
 }
