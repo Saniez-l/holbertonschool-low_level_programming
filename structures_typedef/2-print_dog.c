ad0dd9b73e9bdfd4c0ffd2e3ce9af0fae8e66b05
Saniez-l <saniezl.christophe@gmail.com>
2025-07-11 16:11:55 +0200

    update if owneer or age is NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,16 +8,20 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->age == 0 || d->owner == NULL)
-	{
-		printf("nil\n");
-	}
-	if (d->name == NULL)
-	{
-		printf("Name: nil\n");
-	}
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age == 0)
+		printf("Age: (nil)\n)");
+	else
+		printf("Age: %.6f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n)");
+	else
+		printf("Owner: %s\n", d->owner);
+
 }
