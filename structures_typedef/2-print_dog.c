d89918e806c15ddaf6283dd28db14bbdca31b961
Saniez-l <saniezl.christophe@gmail.com>
2025-07-11 16:38:44 +0200

    update exit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,20 +8,19 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		exit(1);
 
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
+		d->name = "(nil)";
 
-	if (d->age == 0)
-		printf("Age: (nil)\n)");
-	else
-		printf("Age: %.6f\n", d->age);
+	printf("Name: %s\n", d->name);
+
+	printf("Age: %.6f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n)");
-	else
-		printf("Owner: %s\n", d->owner);
+		d->owner = "(nil)";
+
+	printf("Owner: %s\n", d->owner);
 
 }
