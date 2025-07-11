5417a5f9bd0d07683f1242920a91e84270c32ad3
Saniez-l <saniezl.christophe@gmail.com>
2025-07-11 11:47:45 +0200

    correction with while for len2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -32,9 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		len2++;
 	}
-	s = malloc(sizeof(char) * (len1 + len2 + 1));
+	s = malloc(sizeof(char) * (len1 + n + 1));
 	if (s == NULL)
 	{
+		free(s);
 		return (NULL);
 	}
 	for (i = 0; i < len1; i++)
