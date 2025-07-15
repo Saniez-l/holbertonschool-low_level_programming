d93c4064c2972a90c8eac3beeeec5fae081555b9
Saniez-l <saniezl.christophe@gmail.com>
2025-07-15 22:48:29 +0200

    update header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
 #ifndef _VARIADIQUE_
 #define _VARIADIQUE_
+#include <stdio.h>
+#include <stdarg.h>
 
-_putchar(char c);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
