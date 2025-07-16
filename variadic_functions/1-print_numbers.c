68ff61fa5577f2df82e3cbeecbc6cf67212f0bbd
Saniez-l <saniezl.christophe@gmail.com>
2025-07-16 10:35:47 +0200

    program print n number and separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - use variadique for sum
+ * @n: number of variadic
+ * @separator: separateur
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list_print;
+	unsigned int i = 0;
+
+
+	va_start(list_print, n);
+	for (; i < n; i++)
+	{
+		printf("%d", va_arg(list_print, unsigned int));
+		printf("%s", separator);
+	}
+	va_end(list_print);
+	printf("\n");
+}
