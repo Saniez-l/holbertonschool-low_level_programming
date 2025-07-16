4e3ad8884b16483ac90b446639fdfa51a534ccc4
Saniez-l <saniezl.christophe@gmail.com>
2025-07-16 12:18:26 +0200

    function for print caractere with variadic type int

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - use variadique for sum
+ * @n: number of variadic
+ * @separator: separateur
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list_print;
+	unsigned int i = 0;
+	char *c;
+
+	va_start(list_print, n);
+
+	for (; i < n; i++)
+	{
+		c = va_arg(list_print, char*);
+		if (c == NULL)
+			printf("(nil)");
+
+		printf("%s", c);
+
+		if (i != n - 1 &&  n != 0)
+			printf("%s", separator);
+	}
+	va_end(list_print);
+	printf("\n");
+}
