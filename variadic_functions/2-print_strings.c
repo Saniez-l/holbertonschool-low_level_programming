9221c7a4d88d99869cfc611358229f69c8877865
Saniez-l <saniezl.christophe@gmail.com>
2025-07-16 13:21:10 +0200

    update condition for serator

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(list_print, n);
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		c = va_arg(list_print, char*);
 		if (c == NULL)
