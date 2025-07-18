6099ecc80f919cf4f0b879c2b75349a8b8ef34a9
Saniez-l <saniezl.christophe@gmail.com>
2025-07-18 10:57:45 +0200

    condition of if str

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -29,7 +29,7 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				str = va_arg(name, char *);
-				if (str == NULL)
+				if (str == NULL || !str)
 					printf("(nil)");
 
 				printf("%s", str);
