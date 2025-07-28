fcd74302f336b3e4b37a5610be94330253efc238
Saniez-l <saniezl.christophe@gmail.com>
2025-07-28 14:33:39 +0200

    function for add new nod ind the head

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include<string.h>
+
+/**
+ * add_node - insert element of list chained
+ *
+ * @head: header element
+ * @str: string
+ * Return: new.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+
+	new->next = *head;
+	new->len = strlen(str);
+	new->str = strdup(str);
+	*head = new;
+	return (new);
+
+}
