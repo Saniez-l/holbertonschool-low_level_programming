6e9306b318f1318da6f69ac47e1a1b6ceda8ebd2
Saniez-l <saniezl.christophe@gmail.com>
2025-07-29 10:55:56 +0200

    function return number of element of doubel list chained

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - print element of list chained
+ *
+ * @h: header element
+ * Return: count
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
