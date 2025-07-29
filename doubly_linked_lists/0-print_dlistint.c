09a33533bc2f1d10494f17d5c9f3d32190dfd83d
Saniez-l <saniezl.christophe@gmail.com>
2025-07-29 10:34:02 +0200

    function for print element double list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print element of list chained
+ *
+ * @h: header element
+ * Return: count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
