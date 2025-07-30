8b2c8b0456ddf3f2bdc8d2c175d4c6d047a25a78
Saniez-l <saniezl.christophe@gmail.com>
2025-07-30 18:35:33 +0200

    function for write with posix

---
+++ file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - read text file
+ * @filename: file name
+ * @text_content: size
+ *
+ * Return: charWrite
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, charWrite = 0;
+
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	charWrite = write(fd, text_content, strlen(text_content));
+	if (charWrite == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+
+}
