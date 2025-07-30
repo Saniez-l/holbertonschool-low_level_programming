551fcf23518f1b326ef24cf5e7eb1b8f1d31ad2f
Saniez-l <saniezl.christophe@gmail.com>
2025-07-30 09:46:32 +0200

    delet one nod with index value index

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *tmp;
+	dlistint_t *prev_nod;
+	dlistint_t *next_nod;
+
+	tmp = *head;
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+		(*head)->prev = NULL;
+
+		free(tmp);
+		return (1);
+
+	}
+	while (count < index)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	if (!tmp)
+		return (-1);
+
+	prev_nod = tmp->prev;
+	next_nod = tmp->next;
+
+	if (prev_nod)
+	{
+		prev_nod->next = next_nod;
+	}
+
+	if (next_nod)
+	{
+		next_nod->prev = prev_nod;
+	}
+
+	free(tmp);
+	return (1);
+}
