dfdd3ab857638b36f0d9dc399e56be7c7ff0d9c3
Saniez-l <saniezl.christophe@gmail.com>
2025-07-30 21:57:04 +0200

    add a condition if filename is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,6 +11,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = 0, charWrite = 0;
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
