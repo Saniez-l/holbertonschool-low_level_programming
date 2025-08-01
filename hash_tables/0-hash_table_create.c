61a9603a4b39981f7c01421e865e514c2ce69ffd
Saniez-l <saniezl.christophe@gmail.com>
2025-08-01 15:03:04 +0200

    function for creat hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,11 +9,28 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t) * size);
+
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
+
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+
+	table->size = size;
+
 	return (table);
 }
